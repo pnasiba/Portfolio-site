e83947e9d5f2f4af48a8ad3142caacfcd14c4ef5
pnasiba <pnasiba23.06@gmail.com>
2023-12-06 22:55:13 +0500

    portfolio site with js

---
+++ main.js
@@ -0,0 +1,12 @@
+window.addEventListener("scroll", function (){
+    shrink();
+});
+
+let navbar = document.getElementById("navbar");
+function shrink(){
+    if(scrollY > 100){
+        navbar.classList.add("navbar-shrink");
+    } else{
+        navbar.classList.remove("navbar-shrink");
+    }
+}
